a61ba7c52b0144da017f0229eaa11d906e7528ca
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-15 10:39:48 -0700

    cleaned up ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,18 +7,13 @@ class RingBuffer:
     def append(self, item):
         if len(self.data) < self.capacity:
             self.data.append(item)
-            print(self.data)
             return self.data
         elif len(self.data) == self.capacity:
-            print('current', self.current)
             self.data[self.current] = item
-            print(self.data)
-
             if self.current == 4:
                 self.current = 0
             else:
                 self.current += 1
- 
 
     def get(self):
         return self.data
